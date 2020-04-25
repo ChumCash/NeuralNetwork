548ee19927daa25d48cb9c8b1e9d777b28f6dd22
ChumCash <64326781+ChumCash@users.noreply.github.com>
2020-04-25 22:05:51 +0300

    Add NeuronNetwork.sln

---
+++ NeuronNetwork/Neuron.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace NeuronNetwork
+{
+    public class Neuron
+    {
+        public List<double> weights { get; }
+        public NeuronTypes neuronType { get; }
+        public double neuronOutput { get; private set; }
+
+        public Neuron(int countNeuron,NeuronTypes type = NeuronTypes.Hidden)
+        {
+            neuronType = type;
+            weights = new List<double>();
+            for(int i = 0;i<weights.Count;i++)
+            {
+                weights.Add(1);
+            }
+        }
+
+        public double FeedForward(List<double> inputs)
+        {
+            var sum = 0.0;
+            for (int i = 0;i<inputs.Count;i++)
+            {
+                sum += inputs[i] * weights[i];
+            }
+            if (neuronType != NeuronTypes.Input)
+                neuronOutput = Sigmoida(sum);
+            else
+                neuronOutput = sum;
+            return neuronOutput;
+        }
+
+        public void SetWeights(params double[] weightsNeuron)
+        {
+            for (int i =0;i<weightsNeuron.Length;i++)
+            {
+                weights[i] = weightsNeuron[i];
+            }
+        }
+
+        public double Sigmoida(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+
+        public override string ToString()
+        {
+            return neuronOutput.ToString();
+        }
+    }
+}
---
+++ NeuronNetwork/NeuronTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuronNetwork
+{
+    public enum NeuronTypes
+    {
+        Input,
+        Hidden,
+        Output
+    }
+}
